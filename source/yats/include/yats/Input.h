bd0de6cec07486d1fce73d71a518b4d11deb7c75
Mathias <mathias.flueggen@student.hpi.de>
2017-11-30 15:10:13 +0100

    Adds basic implementation for input & output class.

--- source/yats/include/yats/Input.h
+++ source/yats/include/yats/Input.h
@@ -1,7 +1,19 @@
 #pragma once
 
-
+template<typename T, size_t Id>
 class Input
 {
+public:
+	using value_type = T;
+	static constexpr size_t ID = Id;
 
+	Input(T value) : m_value{value} {}
+
+	operator const T&() const { return m_value; }
+	operator T&() { return m_value; }
+	//T& operator*() { return m_value; }
+protected:
+	T m_value;
+private:
+		
 };

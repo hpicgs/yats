871a7df3c777c1376786afac920e9e9809ee505e
Jan van Dieken <Zanthelo@users.noreply.github.com>
2017-12-01 13:41:01 +0100

    changes ASSERT_* to EXPECT_*

--- source/tests/yats-test/util_test.cpp
+++ source/tests/yats-test/util_test.cpp
@@ -7,40 +7,40 @@
 
 TEST(util_test, is_unique_ptr_test)
 {
-	ASSERT_TRUE(is_unique_ptr_v<std::unique_ptr<int>>);
-	ASSERT_FALSE(is_unique_ptr_v<std::shared_ptr<int>>);
-	ASSERT_FALSE(is_unique_ptr_v<int>);
-	ASSERT_FALSE(is_unique_ptr_v<int*>);
-	ASSERT_FALSE(is_unique_ptr_v<int&>);
-	ASSERT_FALSE(is_unique_ptr_v<const int&>);
-	ASSERT_FALSE(is_unique_ptr_v<void>);
+	EXPECT_TRUE(is_unique_ptr_v<std::unique_ptr<int>>);
+	EXPECT_FALSE(is_unique_ptr_v<std::shared_ptr<int>>);
+	EXPECT_FALSE(is_unique_ptr_v<int>);
+	EXPECT_FALSE(is_unique_ptr_v<int*>);
+	EXPECT_FALSE(is_unique_ptr_v<int&>);
+	EXPECT_FALSE(is_unique_ptr_v<const int&>);
+	EXPECT_FALSE(is_unique_ptr_v<void>);
 
 	struct no_unique_ptr
 	{
 		char dummy;
 	};
 
-	ASSERT_FALSE(is_unique_ptr_v<no_unique_ptr>);
-	ASSERT_FALSE(is_unique_ptr_v<no_unique_ptr*>);
+	EXPECT_FALSE(is_unique_ptr_v<no_unique_ptr>);
+	EXPECT_FALSE(is_unique_ptr_v<no_unique_ptr*>);
 }
 
 TEST(util_test, is_shared_ptr_test)
 {
-	ASSERT_TRUE(is_shared_ptr_v<std::shared_ptr<int>>);
-	ASSERT_FALSE(is_shared_ptr_v<std::unique_ptr<int>>);
-	ASSERT_FALSE(is_shared_ptr_v<int>);
-	ASSERT_FALSE(is_shared_ptr_v<int*>);
-	ASSERT_FALSE(is_shared_ptr_v<int&>);
-	ASSERT_FALSE(is_shared_ptr_v<const int&>);
-	ASSERT_FALSE(is_shared_ptr_v<void>);
+	EXPECT_TRUE(is_shared_ptr_v<std::shared_ptr<int>>);
+	EXPECT_FALSE(is_shared_ptr_v<std::unique_ptr<int>>);
+	EXPECT_FALSE(is_shared_ptr_v<int>);
+	EXPECT_FALSE(is_shared_ptr_v<int*>);
+	EXPECT_FALSE(is_shared_ptr_v<int&>);
+	EXPECT_FALSE(is_shared_ptr_v<const int&>);
+	EXPECT_FALSE(is_shared_ptr_v<void>);
 
 	struct no_shared_ptr
 	{
 		char dummy;
 	};
 
-	ASSERT_FALSE(is_unique_ptr_v<no_shared_ptr>);
-	ASSERT_FALSE(is_unique_ptr_v<no_shared_ptr*>);
+	EXPECT_FALSE(is_unique_ptr_v<no_shared_ptr>);
+	EXPECT_FALSE(is_unique_ptr_v<no_shared_ptr*>);
 }
 
 TEST(util_test, has_run_test)
@@ -60,7 +60,7 @@ TEST(util_test, has_run_test)
 		int i;
 	};
 
-	ASSERT_TRUE(has_run_v<member_run>);
-	ASSERT_TRUE(has_run_v<static_run>);
-	ASSERT_FALSE(has_run_v<no_run>);
+	EXPECT_TRUE(has_run_v<member_run>);
+	EXPECT_TRUE(has_run_v<static_run>);
+	EXPECT_FALSE(has_run_v<no_run>);
 }

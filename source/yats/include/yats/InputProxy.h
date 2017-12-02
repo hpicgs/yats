4bfd02f21fd2fc8d8272819747865f5cc96031e9
Mathias <mathias.flueggen@student.hpi.de>
2017-12-02 21:57:09 +0100

    Adds a litte documentation to InputProxy.

--- source/yats/include/yats/InputProxy.h
+++ source/yats/include/yats/InputProxy.h
@@ -6,13 +6,17 @@
 
 class OutputProxy;
 
-// Allows the connection of an output to the input.
+/// <summary>Allows connecting an output (the source) to an input (the target).</summary>
+/// <remarks>Each input may only be connected to one output.</remarks>
 class InputProxy
 {
 public:
 	InputProxy() : m_output(nullptr) {}
 	InputProxy(const InputProxy& input) = delete;
 
+	/// <summary>Connects output to input.</summary>
+	/// <param name="output">Reference to output to connect.</param>
+	/// <exception cref="logic_error">Thrown when input is already connected to other output.</exception>
 	void operator<<(OutputProxy& output)
 	{
 		if (m_output != nullptr)

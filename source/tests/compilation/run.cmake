d70514193a95c5fd502b1cf7c53bb5ea6d82038f
Highlife1911 <Highlife1911@github.com>
2018-01-01 13:37:38 +0100

    Fix failing builds.

--- source/tests/compilation/run.cmake
+++ source/tests/compilation/run.cmake
@@ -1,4 +1,4 @@
-execute_process(COMMAND "${CMAKE_COMMAND}" --build . --target ${CMAKE_ARGV3} RESULT_VARIABLE res_var ERROR_QUIET)
+execute_process(COMMAND "${CMAKE_COMMAND}" --build . --target ${CMAKE_ARGV3} RESULT_VARIABLE res_var OUTPUT_QUIET)
 if("${res_var}" STREQUAL "0")
   message(FATAL_ERROR "Compilation did not fail!")
 endif()

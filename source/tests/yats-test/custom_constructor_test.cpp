cb74c18208561de8fdc0c78235d394f20148b190
Jan van Dieken <Zanthelo@users.noreply.github.com>
2018-01-16 20:37:46 +0100

    adds test to make sure no unneeded copies are done

--- source/tests/yats-test/custom_constructor_test.cpp
+++ source/tests/yats-test/custom_constructor_test.cpp
@@ -54,3 +54,54 @@ TEST(custom_constructor_test, reference_as_argument)
     scheduler.run();
     EXPECT_EQ(start_value, end_value);
 }
+
+struct constructor_counter
+{
+    constructor_counter() = default;
+    constructor_counter(const constructor_counter& rhs)
+    {
+        copied = rhs.copied + 1;
+        moved = rhs.moved;
+    }
+
+    constructor_counter(constructor_counter&& rhs)
+    {
+        copied = rhs.copied;
+        moved = rhs.moved + 1;
+    }
+
+    uint8_t copied = 0;
+    uint8_t moved = 0;
+};
+
+TEST(custom_constructor_test, no_unnecessary_copy)
+{
+    struct Task
+    {
+        Task(uint8_t& c, constructor_counter cc)
+            : copied(c)
+            , counter(std::move(cc))
+        {
+        }
+
+        void run()
+        {
+            copied = counter.copied;
+        }
+
+        uint8_t& copied;
+        constructor_counter counter;
+    };
+
+    yats::pipeline pipeline;
+
+    uint8_t copy_counter = 0;
+    pipeline.add<Task>(copy_counter, constructor_counter());
+
+    // This should be the only place, where the copy constructor is called
+    // Also it should be called only once
+    yats::scheduler scheduler(pipeline);
+    scheduler.run();
+
+    EXPECT_EQ(copy_counter, 1);
+}

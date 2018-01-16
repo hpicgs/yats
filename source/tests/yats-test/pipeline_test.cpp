1ef44f70f352ae8a40ab63ed9717a8af55e0aa2c
Zanthelo <Zanthelo@users.noreply.github.com>
2018-01-16 14:54:54 +0100

    changes variable names

--- source/tests/yats-test/pipeline_test.cpp
+++ source/tests/yats-test/pipeline_test.cpp
@@ -8,14 +8,14 @@ TEST(pipeline_test, add_lambda_task)
 {
     yats::pipeline pipeline;
 
-    int ss = 25;
-    auto lambda_source = pipeline.add([&ss]() -> yats::slot<int, 1> { return 30; });
-    auto lambda_target = pipeline.add([&ss](yats::slot<int, 1> s) mutable { ss = s; });
+    int test_int = 25;
+    auto lambda_source = pipeline.add([]() -> yats::slot<int, 1> { return 30; });
+    auto lambda_target = pipeline.add([&test_int](yats::slot<int, 1> value) mutable { test_int = value; });
 
     lambda_source->output<1>() >> lambda_target->input<1>();
 
     yats::scheduler scheduler(pipeline);
     scheduler.run();
 
-    EXPECT_EQ(ss, 30);
+    EXPECT_EQ(test_int, 30);
 }

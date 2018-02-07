ff522a76a0d8480805f0a7ac353f465ca3433735
Mathias Flüggen <mathias.flueggen@student.hpi.de>
2018-02-07 18:09:57 +0100

    New tasks are added in the thread of the scheduler.

--- source/yats/include/yats/scheduler.h
+++ source/yats/include/yats/scheduler.h
@@ -8,8 +8,8 @@
 #include <vector>
 
 #include <yats/pipeline.h>
-#include <yats/task_configurator.h>
 #include <yats/thread_pool.h>
+#include <yats/task_container.h>
 
 namespace yats
 {
@@ -17,14 +17,15 @@ namespace yats
 class scheduler
 {
 public:
-    explicit scheduler(const pipeline& pipeline)
-        : scheduler(pipeline, 4)
-    {
-    }
-
-    explicit scheduler(const pipeline& pipeline, const size_t number_of_threads)
+    explicit scheduler(const pipeline& pipeline, const size_t number_of_threads = 4)
         : m_tasks(pipeline.build()), m_thread_pool(number_of_threads)
     {
+        const std::function<void(abstract_task_container*)> f = [this](abstract_task_container* bla)
+        {
+            task_executed(bla);
+        };
+       
+        m_thread_pool.subscribe(f);
     }
 
     scheduler(const scheduler& other) = delete;
@@ -37,7 +38,6 @@ public:
 
     void run()
     {
-        
         for (auto& elem : m_tasks)
         {
             to_run.push_back(elem.get());
@@ -45,52 +45,58 @@ public:
 
         check_runnable();
 
-        while (true)
+        while (!to_run.empty())
         {
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            abstract_task_container* task;
             {
-                std::lock_guard<std::mutex> lock(m_mutex);
-                if (to_run.empty())
-                {
-                    break;
-                }
-            }
-        }
+                std::unique_lock<std::mutex> lock(m_mutex);
+                m_wait_for_task_executed.wait(lock, [this] {return !m_tasks_to_process.empty(); });
 
-        m_thread_pool.wait();
+                task = m_tasks_to_process.front();
+                m_tasks_to_process.pop();
+            }
+
+
+            to_run.erase(std::remove(to_run.begin(), to_run.end(), task));
+
+            check_runnable(task);
+        }
+        ASSERT_EQ(m_tasks_to_process.empty(), true);
     }
+
+
 
 protected:
     // Stores all task_containers with their position as an implicit id
     std::vector<std::unique_ptr<abstract_task_container>> m_tasks;
     yats::thread_pool m_thread_pool;
     std::vector<abstract_task_container*> to_run;
+    std::condition_variable m_wait_for_task_executed;
+    std::queue<abstract_task_container*> m_tasks_to_process;
     std::mutex m_mutex;
 
     void check_runnable()
     {
-        while (true)
-        {
-            const auto runnable = std::find_if(to_run.begin(), to_run.end(), [](abstract_task_container* task) {
-                return task->can_run();
-            });
-
-            if (runnable == to_run.end())
+        for (auto & task : to_run) {
+            if (task->can_run())
             {
-                return;
+                m_thread_pool.execute(task);
             }
-            auto task = *runnable;
-            to_run.erase(runnable);
-            m_thread_pool.execute([task] {task->run(); }, [this](void* v) {task_executed(v); }, task);
         }
-    };
+    }
 
-    void task_executed(void * object)
+    void check_runnable(abstract_task_container* task)
     {
-        std::lock_guard<std::mutex> lock(m_mutex);
-        auto task = static_cast<abstract_task_container*>(object);
-        std::cout << "task executed!" << std::endl;
+        // Add task.following_nodes() to to_run.
         check_runnable();
     }
+
+    void task_executed(abstract_task_container* task)
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_tasks_to_process.push(task);
+        m_wait_for_task_executed.notify_one();
+    }
+
 };
 }

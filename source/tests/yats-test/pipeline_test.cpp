f75189264e7deb97af01f9ed51fad328ee0a5f26
mflueggen <mflueggen@uos.de>
2018-02-16 12:14:57 +0100

    Adresses issue 101. Uses namespace yats in tests and demos. (#125)
    * Adresses issue 101. Uses namespace yats in tests and demos.

--- source/tests/yats-test/pipeline_test.cpp
+++ source/tests/yats-test/pipeline_test.cpp
@@ -4,19 +4,19 @@
 #include <yats/scheduler.h>
 #include <yats/slot.h>
 
-#include <test_util.h>
+using namespace yats;
 
 TEST(pipeline_test, add_lambda_task)
 {
-    yats::pipeline pipeline;
+    pipeline pipeline;
 
     int test_int = 25;
-    auto lambda_source = pipeline.add([]() -> yats::slot<int, 1> { return 30; });
-    auto lambda_target = pipeline.add([&test_int](yats::slot<int, 1> value) mutable { test_int = value; });
+    auto lambda_source = pipeline.add([]() -> slot<int, 1> { return 30; });
+    auto lambda_target = pipeline.add([&test_int](slot<int, 1> value) mutable { test_int = value; });
 
     lambda_source->output<1>() >> lambda_target->input<1>();
 
-    yats::scheduler scheduler(std::move(pipeline));
+    scheduler scheduler(std::move(pipeline));
     scheduler.run();
 
     EXPECT_EQ(test_int, 30);
@@ -24,14 +24,14 @@ TEST(pipeline_test, add_lambda_task)
 
 TEST(pipeline_test, add_one_listener)
 {
-    yats::pipeline pipeline;
+    pipeline pipeline;
 
-    auto lambda_source = pipeline.add([]() -> yats::slot<int, 0> { return 30; });
+    auto lambda_source = pipeline.add([]() -> slot<int, 0> { return 30; });
 
     int output = 0;
     lambda_source->add_listener<0>([&output](int value) { output = value; });
 
-    yats::scheduler scheduler(std::move(pipeline));
+    scheduler scheduler(std::move(pipeline));
     scheduler.run();
 
     EXPECT_EQ(output, 30);
@@ -39,17 +39,17 @@ TEST(pipeline_test, add_one_listener)
 
 TEST(pipeline_test, add_listener_and_task)
 {
-    yats::pipeline pipeline;
+    pipeline pipeline;
 
-    auto lambda_source = pipeline.add([]() -> yats::slot<int, 0> { return 30; });
-    auto lambda_target = pipeline.add([](yats::slot<int, 0>) {});
+    auto lambda_source = pipeline.add([]() -> slot<int, 0> { return 30; });
+    auto lambda_target = pipeline.add([](slot<int, 0>) {});
 
     int output = 0;
     lambda_source->add_listener<0>([&output](int value) { output = value; });
 
     lambda_source->output<0>() >> lambda_target->input<0>();
 
-    yats::scheduler scheduler(std::move(pipeline));
+    scheduler scheduler(std::move(pipeline));
     scheduler.run();
 
     EXPECT_EQ(output, 30);
@@ -57,27 +57,27 @@ TEST(pipeline_test, add_listener_and_task)
 
 TEST(pipeline_test, add_no_listener)
 {
-    yats::pipeline pipeline;
+    pipeline pipeline;
 
-    auto lambda_source = pipeline.add([]() -> yats::slot<int, 0> { return 30; });
+    auto lambda_source = pipeline.add([]() -> slot<int, 0> { return 30; });
     (void) lambda_source;
 
-    yats::scheduler scheduler(std::move(pipeline));
+    scheduler scheduler(std::move(pipeline));
     scheduler.run();
 }
 
 TEST(pipeline_test, add_multiple_listener)
 {
-    yats::pipeline pipeline;
+    pipeline pipeline;
 
-    auto lambda_source = pipeline.add([]() -> yats::slot<int, 0> { return 30; });
+    auto lambda_source = pipeline.add([]() -> slot<int, 0> { return 30; });
 
     int output1 = 0;
     int output2 = 0;
     lambda_source->add_listener<0>([&output1](int value) { output1 = value; });
     lambda_source->add_listener<0>([&output2](int value) { output2 = value; });
 
-    yats::scheduler scheduler(std::move(pipeline));
+    scheduler scheduler(std::move(pipeline));
     scheduler.run();
 
     EXPECT_EQ(output1, 30);
--- source/tests/yats-test/yats_test.cpp
+++ source/tests/yats-test/yats_test.cpp
@@ -3,9 +3,11 @@
 #include <yats/slot.h>
 #include <yats/scheduler.h>
 
+using namespace yats;
+
 struct Source
 {
-    yats::output_bundle<yats::slot<int, 0>> run()
+    output_bundle<slot<int, 0>> run()
     {
         std::cout << "Send 42" << std::endl;
         return std::make_tuple(42);
@@ -14,7 +16,7 @@ struct Source
 
 struct Target
 {
-    void run(yats::slot<int, 0> input)
+    void run(slot<int, 0> input)
     {
         std::cout << "Received " << input << ". Adding + 1" << std::endl;
     }
@@ -22,20 +24,20 @@ struct Target
 
 TEST(yats_test, simple)
 {
-    yats::pipeline pipeline;
-    yats::scheduler scheduler(std::move(pipeline));
+    pipeline pipeline;
+    scheduler scheduler(std::move(pipeline));
     EXPECT_NO_THROW(scheduler.run());
 }
 
 TEST(yats_test, simple_connection)
 {
-    yats::pipeline pipeline;
+    pipeline pipeline;
 
     auto source_configurator = pipeline.add<Source>();
     auto target_configurator = pipeline.add<Target>();
 
     source_configurator->output<0>() >> target_configurator->input<0>();
 
-    yats::scheduler scheduler(std::move(pipeline));
+    scheduler scheduler(std::move(pipeline));
     scheduler.run();
 }

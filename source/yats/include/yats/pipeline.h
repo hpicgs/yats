5b77bb0723dfbe374efd7ebd2c70e27f7eddccf1
Mathias Flüggen <mathias.flueggen@student.hpi.de>
2018-03-08 17:40:48 +0100

    Adds comments.

--- source/yats/include/yats/pipeline.h
+++ source/yats/include/yats/pipeline.h
@@ -27,6 +27,8 @@ public:
             m_helpers.emplace_back(configurator->construct_connection_helper());
         }
 
+        // collects all outputs and stores the index of the helper to the
+        // corresponding output.
         for (size_t i = 0; i < m_tasks->size(); ++i)
         {
             auto outputs = m_helpers[i]->outputs();
@@ -37,6 +39,10 @@ public:
         }
     }
 
+    /**
+     * Advances the iterator to the next input if possible.
+     * @return true, if the iterator could be advanced. false otherwise.
+     */
     bool next()
     {
         if (advance_to_next_input())
@@ -55,17 +61,27 @@ public:
         return false;
     }
 
+    /**
+     * Returns a pointer to the current input_connector.
+     */
     const abstract_input_connector* input() const
     {
         return m_input_iterator->first;
     }
 
+    /**
+     * Returns the index of the current input in the run function
+     */
     size_t index_of_input() const
     {
         return m_input_iterator->second;
     }
 
-    size_t source_index() const
+    /**
+     * Returns the index of the helper associated to the source for the current input
+     * Returns std::numeric_limits<size_t>::max() if there is no source to the input.
+     */
+    size_t source_helper_index() const
     {
         if (source() != nullptr)
         {
@@ -74,36 +90,59 @@ public:
         return std::numeric_limits<size_t>::max();
     }
 
+    /**
+     * Returns a pointer to the output_connector which is the source for the current input.
+     * This can be a nullptr.
+     */
     const abstract_output_connector* source() const
     {
         return input()->output();
     }
 
+    /**
+     * Returns the currently selected helper
+     */
     abstract_connection_helper* get_helper() const
     {
-        return m_helpers[m_helper_index].get();
+        return m_helpers[helper_index()].get();
     }
 
+    /**
+     * Returns a pointer to the helper with {@code index}
+     * @param index Index of helper to return
+     */
     abstract_connection_helper* get_helper(size_t index) const
     {
         return m_helpers[index].get();
     }
 
+    /**
+     * Moves helpers to the caller
+     */
     std::vector<std::unique_ptr<abstract_connection_helper>> take_helpers()
     {
         return std::move(m_helpers);
     }
 
+    /**
+     * Returns the index of the current helper
+     */
     size_t helper_index() const
     {
         return m_helper_index;
     }
 
+    /**
+     * Returns the index of the helper associated to {@code output},
+     */
     size_t get_helper_index_to_output(const abstract_output_connector* output) const
     {
         return m_output_owners.at(output);
     }
 
+    /**
+     * Returns the numer of helpers.
+     */
     size_t helper_count() const
     {
         return m_helpers.size();
@@ -118,6 +157,10 @@ protected:
     std::map<const abstract_output_connector*, size_t> m_output_owners;
     bool m_iterator_changed;
 
+    /**
+     * Advances to the next helper.
+     * @return Returns true if the iterator could be advanced. Returns false otherwise.
+     */
     bool advance_to_next_helper()
     {
         ++m_helper_index;
@@ -131,6 +174,10 @@ protected:
         return true;
     }
 
+    /**
+     * Advances to the next input in the current helper.
+     * @return returns true, if another input could be selected. Returns false otherwise.
+     */
     bool advance_to_next_input()
     {
         if (m_helper_index >= m_helpers.size())
@@ -192,7 +239,7 @@ public:
         
         while(iter.next())
         {
-            auto helper = iter.get_helper(iter.source_index());
+            auto helper = iter.get_helper(iter.source_helper_index());
             helper->bind(iter.source(), iter.get_helper()->target(iter.input()));
             helper->add_following(iter.helper_index());
         }
@@ -211,7 +258,7 @@ public:
      * Saves the pipeline to a file in the Graphviz DOT format.
      * @param filename Filename to save the pipeline to.
      */
-    void save_to_file(const std::string& filename)
+    void save_to_file(const std::string& filename) const
     {
         io_iterator iter(&m_tasks);
 
@@ -224,7 +271,9 @@ public:
         file << '\t' << "rankdir = LR;" << std::endl << std::endl;
         file << '\t' << "node [shape = record];" << std::endl;
 
-        // Creates a vertex for each task including the task's inputs and outputs
+        // Creates a vertex for each task including the task's inputs and outputs.
+        // The tasks are named n0, n1, ... according to their helper index. They are labeled with their class name.
+        // Key for input values are i1, i2, .... Keys for out values are o1, o2, ... .
         // NODE_NAME [label = "NODE_NAME|{{<KEY1>INPUT1|<KEY2>INPUT2...}|{<KEY3>OUTPUT1|<KEY4>OUTPUT2...}}"];
         for (size_t i = 0; i < iter.helper_count(); ++i)
         {
@@ -236,6 +285,7 @@ public:
 
         file << std::endl;
 
+        // We need to keep track of outputs which are not connected.
         std::set<const abstract_output_connector*> unused_outputs;
         for (size_t i = 0; i < m_tasks.size(); ++i)
         {
@@ -246,6 +296,8 @@ public:
             }
         }
         
+        // Counter to name either inputs not fed by an output or outputs not connected by an
+        // input.
         auto id_counter = 0;
 
         while (iter.next())
@@ -257,15 +309,17 @@ public:
                 file << 'u' << id_counter << ';' << std::endl;
                 file << '\t' << 'u' << id_counter << "->" << 'n' << iter.helper_index() << ':' << "<i" << iter.index_of_input() << '>' << std::endl;
                 ++id_counter;
-            }
+            } // normal case. Adds edge from the output connected to the current input.
             else
             {
-                file << '\t' << 'n' << iter.source_index() << ':' << "<o" << iter.get_helper(iter.source_index())->get_output_index(iter.source());
+                file << '\t' << 'n' << iter.source_helper_index() << ':' << "<o";
+                file << iter.get_helper(iter.source_helper_index())->get_output_index(iter.source());
                 file << "> -> " << 'n' << iter.helper_index() << ':' << "<i" << iter.index_of_input() << '>' << std::endl;
                 unused_outputs.erase(iter.source());
             }
         }
 
+        // Adds outputs which are not connected to an input.
         for (const auto& output : unused_outputs)
         {
             const auto helper_index = iter.get_helper_index_to_output(output);
@@ -281,6 +335,13 @@ public:
         file.close();
     }
 
+    /**
+     * Creates a string of all inputs in {@code helper} of the format <KEY1>INPUT1|<KEY2>INPUT2...
+     * Keys are of the format <i0>, <i1>, etc.
+     * The order of the inputs depends on their placement in the underlying map in {@code helper}.
+     * @param helper Helper whose inputs are to by converted to a string.
+     * @return A string containing all inputs according to the format described above
+     */
     static std::string inputs_to_string(abstract_connection_helper& helper)
     {
         std::stringstream input_id_stream;
@@ -301,6 +362,13 @@ public:
         return tmp;
     }
 
+    /**
+    * Creates a string of all outputs in {@code helper} of the format <KEY1>OUTPUT1|<KEY2>OUTPUT2...
+    * Keys are of the format <o0>, <o1>, etc.
+    * The order of the inputs depends on their placement in the underlying map in {@code helper}.
+    * @param helper Helper whose outputs are to by converted to a string.
+    * @return A string containing all outputs according to the format described above
+    */
     static std::string outputs_to_string(abstract_connection_helper& helper)
     {
         std::stringstream output_id_stream;

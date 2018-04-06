63728e67b7c6b37725b6936ca357c77b0ae0d756
Jan van Dieken <Zanthelo@users.noreply.github.com>
2018-04-06 11:45:40 +0200

    adds public interface comments for pipeline, scheduler and task_configurator

--- source/yats/include/yats/identifier.h
+++ source/yats/include/yats/identifier.h
@@ -15,13 +15,13 @@ class identifier
 {
 public:
     /**
-    * Converts {@code character} to an unsigned int.
-    * Supported characters are [a-z],[A-Z], ' ', '*', '/', '&', '#' and '_'.
-    * Other characters wil be treated like '_'. Characters [a-z] and [A-Z]
-    * are represented by the same unsigned int.
-    * @param character Character to convert
-    * @return Numeric representation of character as 64-bit unsigned int-
-    */
+     * Converts {@code character} to an unsigned int.
+     * Supported characters are [a-z],[A-Z], ' ', '*', '/', '&', '#' and '_'.
+     * Other characters wil be treated like '_'. Characters [a-z] and [A-Z]
+     * are represented by the same unsigned int.
+     * @param character Character to convert
+     * @return Numeric representation of character as 64-bit unsigned int-
+     */
     static constexpr uint64_t char_to_int(char character)
     {
         // Upper case
